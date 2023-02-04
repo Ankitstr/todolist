84956434e9c3c20ff23bd5b5133e1bfe1e7a7a8b
Ankitstr <124289461+Ankitstr@users.noreply.github.com>
2023-02-04 10:29:03 +0530

    index.html

---
+++ aa.js
@@ -0,0 +1,13 @@
+// var promise = new Promise((resolve ,reject) =>{
+//     setTimeout(() =>{
+//         resolve({message:'success'});
+//     },3000);
+// });
+// console.log(promise);
+
+console.log(1);
+setTimeout(function(){
+    console.log(2);
+},0);
+console.log(3);
+console.log("gfgf");
